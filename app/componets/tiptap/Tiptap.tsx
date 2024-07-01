b9be1974d191628bde085f4b05d8512c84aee360
Balyeet1 <ricardoldias123@gmail.com>
2024-07-01 10:20:08 +0000

    Refactor tiptap component.

--- app/componets/tiptap/Tiptap.tsx
+++ app/componets/tiptap/Tiptap.tsx
@@ -1,4 +1,4 @@
-import { useEditor, EditorContent, BubbleMenu } from '@tiptap/react'
+import { useEditor, EditorContent, Editor } from '@tiptap/react'
 import Document from '@tiptap/extension-document'
 import { createLowlight } from 'lowlight';
 import CodeBlockLowlight from '@tiptap/extension-code-block-lowlight'
@@ -14,96 +14,70 @@ import FontFamily from '@tiptap/extension-font-family';
 import TextStyle from '@tiptap/extension-text-style'
 import FontSize from 'tiptap-extension-font-size';
 
-interface CustomImageConfig {
-    src: { default: null };
-    alt: { default: null };
-    title: { default: null };
-    width: { default: string };
-    alignment: { default: string };
-    float: { default: string };
-    editable?: boolean;
-}
-
-const CustomDocument = Document.extend({
-    content: 'heading block*',
-})
-
-const lowlight = createLowlight()
-lowlight.register("js", js)
-
 
 const Tiptap = ({ className, content, onChange, isReadonly }: { className?: string, content?: string, onChange?: any, isReadonly: boolean }) => {
 
-    //const ImageExtention = isReadonly ? Image : CustomImage
+    const CustomDocument = Document.extend({
+        content: 'heading block*',
+    })
+
+    const lowlight = createLowlight()
+    lowlight.register("js", js)
+
 
     const editor = useEditor({
         editable: !isReadonly,
-        editorProps: {
-            attributes: {
-                class: className || ""
-            }
-        },
-        onUpdate: ({ editor }) => {
-            const htmlContent = editor.getHTML();
-            onChange(htmlContent)
-
-        },
+        editorProps: { attributes: { class: className || "" } },
+        onUpdate: ({ editor }) => onChange(editor.getHTML()),
         extensions: [
-            StarterKit,
-            SmilieReplacer,
+            // Core Extensions
+            StarterKit, SmilieReplacer, CustomDocument, Highlight, TextStyle, FontSize,
+
+            // Extensions with configs
             CustomImage.extend({
                 addAttributes() {
-
-                    const configs: CustomImageConfig = {
+                    return {
                         src: { default: null },
                         alt: { default: null },
                         title: { default: null },
                         width: { default: '100%' },
                         alignment: { default: 'center' },
                         float: { default: "none" },
+                        ...(isReadonly ? {} : { editable: true }),
                     }
-
-                    if (!isReadonly) {
-                        configs.editable = true
-                    }
-
-                    return configs
-                }
-            }),
-            CustomDocument,
-            Highlight,
-            TextStyle,
-            FontSize,
-            FontFamily.configure({
-                types: ['textStyle'],
-            }),
-            Placeholder.configure({
-                placeholder: ({ node }) => {
-                    if (node.type.name === 'heading') {
-                        return 'What’s the title?'
-                    }
-
-                    return ''
                 },
             }),
-            CodeBlockLowlight.configure({
-                lowlight: lowlight
-            }),
-            TextAlign.configure({
-                types: ['heading', 'paragraph'],
-            }),
-            Dropcursor.configure({
-                color: '#ff0000',
-                width: 1,
-            }),
-        ],
-        content: content,
 
-    })
+            FontFamily.configure({ types: ['textStyle'] }),
+
+            Placeholder.configure({
+                placeholder: ({ node }) => node.type.name === 'heading' ? 'What’s the title?' : '',
+            }),
+
+            CodeBlockLowlight.configure({ lowlight }),
+
+            TextAlign.configure({ types: ['heading', 'paragraph'] }),
+
+            Dropcursor.configure({ color: '#ff0000', width: 1 }),
+        ],
+        content,
+    });
 
     if (!editor) {
+        console.log("Something went wrong with the editor!")
         return null
     }
+
+    return (
+        <div>
+            {!isReadonly && <Toolbar editor={editor} />}
+            <EditorContent editor={editor} />
+        </div >
+    )
+}
+
+
+const Toolbar = ({ editor }: { editor: Editor }) => {
 
     const addImage = () => {
         const url = window.prompt('URL')
@@ -112,123 +86,131 @@ const Tiptap = ({ className, content, onChange, isReadonly }: { className?: stri
         commands.setImage({ src: url })
     }
 
+    const getAlignement = (): string => {
+        return editor.isActive({ textAlign: 'left' }) ? 'left'
+            : editor.isActive({ textAlign: 'center' }) ? 'center'
+                : editor.isActive({ textAlign: 'right' }) ? 'right'
+                    : "left"
+    }
+
+    
     return (
-        <div>
-            {!isReadonly && <div className="flex justify-between border border-white mb-5 cols-3 grid grid-cols-8 gap-2 sticky top-0 z-10 bg-white">
-                <button onClick={addImage}>Add image from URL</button>
-                <button onClick={() => editor.chain().focus().setTextAlign('left').run()} className={editor.isActive({ textAlign: 'left' }) ? 'is-active' : ''}>
-                    Left
-                </button>
-                <button onClick={() => editor.chain().focus().setTextAlign('center').run()} className={editor.isActive({ textAlign: 'center' }) ? 'is-active' : ''}>
-                    Center
-                </button>
-                <button onClick={() => editor.chain().focus().setTextAlign('right').run()} className={editor.isActive({ textAlign: 'right' }) ? 'is-active' : ''}>
-                    Right
-                </button>
-                <button
-                    onClick={() => editor.chain().focus().setCodeBlock().run()}
-                    disabled={editor.isActive('codeBlock')}
-                >
-                    Set code block
-                </button>
-                <button onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()} className={editor.isActive('heading', { level: 1 }) ? 'is-active' : ''}>
-                    H1
-                </button>
-                <button onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()} className={editor.isActive('heading', { level: 2 }) ? 'is-active' : ''}>
-                    H2
-                </button>
-                <button onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()} className={editor.isActive('heading', { level: 3 }) ? 'is-active' : ''}>
-                    H3
-                </button>
-                <button onClick={() => editor.chain().focus().toggleHighlight().run()} className={editor.isActive('highlight') ? 'is-active' : ''}>
-                    Highlight
-                </button>
-                <button
-                    onClick={() => editor.chain().focus().toggleBold().run()}
-                    className={editor.isActive('bold') ? 'is-active' : ''}
-                >
-                    Bold
-                </button>
-                <button
-                    onClick={() => editor.chain().focus().toggleItalic().run()}
-                    className={editor.isActive('italic') ? 'is-active' : ''}
-                >
-                    Italic
-                </button>
-                <button
-                    onClick={() => editor.chain().focus().toggleStrike().run()}
-                    className={editor.isActive('strike') ? 'is-active' : ''}
-                >
-                    Strike
-                </button>
-                <select
-                    className='is-active'
-                    onChange={(e) => editor.chain().focus().setFontFamily(e.target.value).run()}
-                    value={editor.isActive('textStyle') ? editor.getAttributes('textStyle').fontFamily : 'monospace'} // Default to 16pt if not active
-                >
-                    <option value="monospace">Monospace</option>
-                    <option value="serif">Serif</option>
-                    <option value="Comic Sans MS, Comic Sans">Comic Sans</option>
-                    <option value="inter">Inter</option>
-                </select>
+        <div className="flex justify-between border border-white mb-5 cols-3 grid grid-cols-8 gap-2 sticky top-0 z-10 bg-white">
+            <button onClick={addImage}>Add image</button>
+            <select
+                className='is-active'
+                onChange={(e) => editor.chain().focus().setTextAlign(e.target.value).run()}
+                value={getAlignement()}
+            >
+                <option value="left">Left</option>
+                <option value="center">Center</option>
+                <option value="right">Right</option>
+            </select>
 
+            <button
+                onClick={() => editor.chain().focus().setCodeBlock().run()}
+                disabled={editor.isActive('codeBlock')}
+            >
+                Set code block
+            </button>
+            <button onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()} className={editor.isActive('heading', { level: 1 }) ? 'is-active' : ''}>
+                H1
+            </button>
+            <button onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()} className={editor.isActive('heading', { level: 2 }) ? 'is-active' : ''}>
+                H2
+            </button>
+            <button onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()} className={editor.isActive('heading', { level: 3 }) ? 'is-active' : ''}>
+                H3
+            </button>
+            <button onClick={() => editor.chain().focus().toggleHighlight().run()} className={editor.isActive('highlight') ? 'is-active' : ''}>
+                Highlight
+            </button>
+            <button
+                onClick={() => editor.chain().focus().toggleBold().run()}
+                className={editor.isActive('bold') ? 'is-active' : ''}
+            >
+                Bold
+            </button>
+            <button
+                onClick={() => editor.chain().focus().toggleItalic().run()}
+                className={editor.isActive('italic') ? 'is-active' : ''}
+            >
+                Italic
+            </button>
+            <button
+                onClick={() => editor.chain().focus().toggleStrike().run()}
+                className={editor.isActive('strike') ? 'is-active' : ''}
+            >
+                Strike
+            </button>
 
-                <select
-                    className='is-active'
-                    onChange={(e) => editor.chain().focus().setFontSize(e.target.value).run()}
-                    value={editor.isActive('textStyle') ? editor.getAttributes('textStyle').fontSize : '16pt'} // Default to 16pt if not active
-                >
-                    <option value="10pt">10pt</option>
-                    <option value="12pt">12pt</option>
-                    <option value="14pt">14pt</option>
-                    <option value="16pt">16pt</option>
-                    <option value="18pt">18pt</option>
-                    <option value="20pt">20pt</option>
-                    <option value="22pt">22pt</option>
-                    <option value="24pt">24pt</option>
-                </select>
+            <select
+                className='is-active'
+                onChange={(e) => editor.chain().focus().setFontFamily(e.target.value).run()}
+                value={editor.isActive('textStyle') ? editor.getAttributes('textStyle').fontFamily : 'monospace'} // Default to 16pt if not active
+            >
+                <option value="monospace">Monospace</option>
+                <option value="serif">Serif</option>
+                <option value="Comic Sans MS, Comic Sans">Comic Sans</option>
+                <option value="inter">Inter</option>
+            </select>
 
-            </div>}
-            {editor && <BubbleMenu editor={editor} shouldShow={({ state, from }) => {
-                const $pos = state.doc.resolve(from)
-                const node = $pos.node($pos.depth);
-                return false //TODO: Remove this line if you want the bubbleMenu to work
-                return node.type.name !== 'doc' && node.type.name !== 'codeBlock'
-            }}
-                updateDelay={50} tippyOptions={{ duration: 100 }}>
-                <div className="bubble-menu">
-                    <button onClick={() => editor.chain().focus().setTextAlign('left').run()} className={editor.isActive({ textAlign: 'left' }) ? 'is-active' : ''}>
-                        Left
-                    </button>
-                    <button onClick={() => editor.chain().focus().setTextAlign('center').run()} className={editor.isActive({ textAlign: 'center' }) ? 'is-active' : ''}>
-                        Center
-                    </button>
-                    <button onClick={() => editor.chain().focus().setTextAlign('right').run()} className={editor.isActive({ textAlign: 'right' }) ? 'is-active' : ''}>
-                        Right
-                    </button>
-                    <button
-                        onClick={() => editor.chain().focus().toggleBold().run()}
-                        className={editor.isActive('bold') ? 'is-active' : ''}
-                    >
-                        Bold
-                    </button>
-                    <button
-                        onClick={() => editor.chain().focus().toggleItalic().run()}
-                        className={editor.isActive('italic') ? 'is-active' : ''}
-                    >
-                        Italic
-                    </button>
-                    <button
-                        onClick={() => editor.chain().focus().toggleStrike().run()}
-                        className={editor.isActive('strike') ? 'is-active' : ''}
-                    >
-                        Strike
-                    </button>
-                </div>
-            </BubbleMenu>}
-            <EditorContent editor={editor} />
-        </div >
+            <select
+                className='is-active'
+                onChange={(e) => editor.chain().focus().setFontSize(e.target.value).run()}
+                value={editor.isActive('textStyle') ? editor.getAttributes('textStyle').fontSize : '16pt'} // Default to 16pt if not active
+            >
+                <option value="10pt">10pt</option>
+                <option value="12pt">12pt</option>
+                <option value="14pt">14pt</option>
+                <option value="16pt">16pt</option>
+                <option value="18pt">18pt</option>
+                <option value="20pt">20pt</option>
+                <option value="22pt">22pt</option>
+                <option value="24pt">24pt</option>
+            </select>
+
+        </div>
     )
 }
 
 export default Tiptap
+
+// ---------------------------------------------------------------BubbleMenu Add if need it---------------------------------------------------
+
+/* editor && <BubbleMenu editor={editor} shouldShow={({ state, from }) => {
+    const $pos = state.doc.resolve(from)
+    const node = $pos.node($pos.depth);
+    return node.type.name !== 'doc' && node.type.name !== 'codeBlock'
+}}
+    updateDelay={50} tippyOptions={{ duration: 100 }}>
+    <div className="bubble-menu">
+        <button onClick={() => editor.chain().focus().setTextAlign('left').run()} className={editor.isActive({ textAlign: 'left' }) ? 'is-active' : ''}>
+            Left
+        </button>
+        <button onClick={() => editor.chain().focus().setTextAlign('center').run()} className={editor.isActive({ textAlign: 'center' }) ? 'is-active' : ''}>
+            Center
+        </button>
+        <button onClick={() => editor.chain().focus().setTextAlign('right').run()} className={editor.isActive({ textAlign: 'right' }) ? 'is-active' : ''}>
+            Right
+        </button>
+        <button
+            onClick={() => editor.chain().focus().toggleBold().run()}
+            className={editor.isActive('bold') ? 'is-active' : ''}
+        >
+            Bold
+        </button>
+        <button
+            onClick={() => editor.chain().focus().toggleItalic().run()}
+            className={editor.isActive('italic') ? 'is-active' : ''}
+        >
+            Italic
+        </button>
+        <button
+            onClick={() => editor.chain().focus().toggleStrike().run()}
+            className={editor.isActive('strike') ? 'is-active' : ''}
+        >
+            Strike
+        </button>
+    </div>
+</BubbleMenu>*/
